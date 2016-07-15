e636707393f07a2e524870a822e14c695ef5bfeb
Landon West <landonwest5@gmail.com>
2016-07-14 17:22:56 -0700

    create dir structure; add first spec

---
+++ spec/game_spec.rb
@@ -0,0 +1,12 @@
+describe Game do
+  describe '#call' do
+
+    context 'when the choices are different' do
+
+      subject(:game) { described_class.new }
+      it 'rock beats scissors' do
+        expect(subject.call('rock', 'scissors')).to eq('rock beats scissors')
+      end
+    end
+  end
+end
